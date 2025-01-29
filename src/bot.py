95026c9985e5f21b995215bcd9806e0a28cd0dc0
leemount96 <mount.lester@gmail.com>
2025-01-28 20:06:51 -0800

    added yields

--- src/bot.py
+++ src/bot.py
@@ -20,32 +20,11 @@ logging.basicConfig(
 )
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text_caps = ' '.join(context.args).upper()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Type /yields to get the top yield opportunities.")
 
 async def yields(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = processor.generate_opportunities_message()
     await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = []
-    results.append(
-        InlineQueryResultArticle(
-            id=str(uuid4()),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    await context.bot.answer_inline_query(update.inline_query.id, results)
 
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
@@ -56,17 +35,8 @@ if __name__ == '__main__':
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
 
-    echo_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
-    application.add_handler(echo_handler)
-
-    caps_handler = CommandHandler('caps', caps)
-    application.add_handler(caps_handler)
-
     yields_handler = CommandHandler('yields', yields)
     application.add_handler(yields_handler)
-
-    inline_caps_handler = InlineQueryHandler(inline_caps)
-    application.add_handler(inline_caps_handler)
 
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
     application.add_handler(unknown_handler)
